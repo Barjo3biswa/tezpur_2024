f46fac1c5baae14758455deeee06542eb5a75da6
Biswajit Laskar <biswajit2013laskar@gmail.com>
2024-05-23 12:14:48 +0530

    sdfsdfsdfsdfs

--- app/Http/Controllers/Admin/AdmitCardControllerNew.php
+++ app/Http/Controllers/Admin/AdmitCardControllerNew.php
@@ -229,6 +229,7 @@ class AdmitCardControllerNew extends Controller
 //    }
 
 
+
    public function generateAdmitCard()
     {
         // update `sub_exam_centers` set one=0, two=0, three=0, four=0, five=0, six=0, seven=0, eight=0, nine=0
