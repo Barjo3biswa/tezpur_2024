d89f7c075e30df50b34e617c9e84ca153a8b970d
Biswajit Laskar <biswajit2013laskar@gmail.com>
2024-05-23 15:56:47 +0530

    sdfgfdg

--- app/Http/Controllers/CommonApplicationController.php
+++ app/Http/Controllers/CommonApplicationController.php
@@ -2342,7 +2342,7 @@ class CommonApplicationController extends Controller
         $exam_centers = ExamCenter::get();
         $center_id = $request->center_name??0;
         
-        $exam_center = ExamCenter::wehre('id',$center_id)->get();
+        $exam_center = ExamCenter::wehre('id',1)->get();
         dd("ok");
         $group = GroupMaster::get();
         return view('admin.admit_card_new.attendence-new-index',compact('exam_center','group'));
