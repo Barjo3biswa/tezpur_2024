7abca2ea43e159a59b51ffabb60293f43dd7f2d8
Biswajit Laskar <biswajit2013laskar@gmail.com>
2024-05-23 16:20:26 +0530

    sdfdsfsdf

--- app/Http/Controllers/Admin/AdmitCardControllerNew.php
+++ app/Http/Controllers/Admin/AdmitCardControllerNew.php
@@ -185,8 +185,8 @@ class AdmitCardControllerNew extends Controller
                                             ->orderby('first_name')->orderby('middle_name')->orderby('last_name');
                                        }])
                                     //    ->where('id','<=',10)
-                                       ->where('id','<=',20)
-                                    //    ->where('id','<=',30)
+                                    //    ->where('id','<=',20)
+                                       ->where('id','<=',30)
                                     //    ->where('id','<=',100)
                                        ->orderBy('center_name')->get(); 
         // dd($exam_centers);
