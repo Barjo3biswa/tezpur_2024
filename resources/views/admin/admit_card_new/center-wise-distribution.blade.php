e78d59f73e66e9e5e0d63db2337fe5e9dbf8294a
Biswajit Laskar <biswajit2013laskar@gmail.com>
2024-05-31 11:16:06 +0530

    dsfsdfsdf

--- resources/views/admin/admit_card_new/center-wise-distribution.blade.php
+++ resources/views/admin/admit_card_new/center-wise-distribution.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layout.auth')
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/datatables.min.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/datatables.min.css') }}"> --}}
     <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.0.2/css/buttons.dataTables.css">
 @endsection
@@ -53,23 +53,23 @@
     </div>
 @endsection
 
-@section('js')
 
-<script src="https://cdn.datatables.net/2.0.8/js/dataTables.js">
-<script src="https://cdn.datatables.net/buttons/3.0.2/js/dataTables.buttons.js">
-<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.dataTables.js">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js">
-<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.html5.min.js">
-<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.print.min.js">
+@section('js')
+<script src="https://cdn.datatables.net/2.0.8/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.0.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.print.min.js"></script>
 <script>
-    new DataTable('#example', {
-        layout: {
-            topStart: {
-                buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
-            }
-        }
+    $(document).ready(function() {
+        $('#example').DataTable({
+            dom: 'Bfrtip',
+            buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
+            ]
+        });
     });
 </script>
 @endsection
