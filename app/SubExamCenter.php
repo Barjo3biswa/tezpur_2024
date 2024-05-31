ca177d6905324a12b09eb0d4441e5af3b4791330
Biswajit Laskar <biswajit2013laskar@gmail.com>
2024-05-31 10:57:34 +0530

    dfsgfdgdfg

--- app/SubExamCenter.php
+++ app/SubExamCenter.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class SubExamCenter extends Model
 {
     use SoftDeletes;
+    protected $guarded = ["id"];
     public function students(){
         return $this->hasMany(AdmitCard::class, 'id','sub_exam_center_id');
     }
