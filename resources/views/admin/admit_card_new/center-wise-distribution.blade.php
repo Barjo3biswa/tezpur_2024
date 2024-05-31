5f9d2b2280698424db6ca691f4ebac7afec6d450
Biswajit Laskar <biswajit2013laskar@gmail.com>
2024-05-31 11:24:52 +0530

    vbnbvnbvn

--- resources/views/admin/admit_card_new/center-wise-distribution.blade.php
+++ resources/views/admin/admit_card_new/center-wise-distribution.blade.php
@@ -11,7 +11,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
-                    <table class="table table-responsive" id="example">
+                    <table class="table table-responsive" id="latest_testing">
                         <thead>                         
                             <tr>
                                 <th>#</th>
@@ -56,21 +56,4 @@
 @endsection
 
 @section('js')
-<script src="https://cdn.datatables.net/2.0.8/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.0.2/js/dataTables.buttons.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.print.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('#example').DataTable({
-            dom: 'Bfrtip',
-            buttons: [
-                'copy', 'csv', 'excel', 'pdf', 'print'
-            ]
-        });
-    });
-</script>
 @endsection
