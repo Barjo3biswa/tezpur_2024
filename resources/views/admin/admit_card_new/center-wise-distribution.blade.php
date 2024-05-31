e73436c2057fa4bb97c356edf579bfe111d2f34c
Biswajit Laskar <biswajit2013laskar@gmail.com>
2024-05-31 11:36:33 +0530

    dfsgdfg

--- resources/views/admin/admit_card_new/center-wise-distribution.blade.php
+++ resources/views/admin/admit_card_new/center-wise-distribution.blade.php
@@ -1,9 +1,6 @@
 @extends('admin.layout.auth')
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('css/datatables.min.css') }}">
-    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.0.2/css/buttons.dataTables.css">
 @endsection
 
 @section('content')
@@ -56,25 +53,4 @@
 @endsection
 
 @section('js')
-<!-- Load jQuery first -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<!-- Load DataTables and buttons extension -->
-<script src="https://cdn.datatables.net/2.0.8/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.0.2/js/dataTables.buttons.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.html5.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/3.0.2/js/buttons.print.min.js"></script>
-
-<script>
-    $(document).ready(function() {
-        $('#example').DataTable({
-            dom: 'Bfrtip',
-            buttons: [
-                'copy', 'csv', 'excel', 'pdf', 'print'
-            ]
-        });
-    });
-</script>
 @endsection
