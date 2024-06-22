0e2422860a90112dc8472c6f0779976f1a290496
Biswajit Laskar <biswajit2013laskar@gmail.com>
2024-06-22 13:56:54 +0530

    sdfsdf

--- resources/views/common/application/index.blade.php
+++ resources/views/common/application/index.blade.php
@@ -312,29 +312,23 @@
                                             @endif
                                         {{-- @endif --}}
                                         {{-- for additional hostel Payment --}}
-                                        @php
-                                            $isre_collect = 0;
-                                            $receipt_re = 0;
-                                            foreach($application->merit_list as $ml){
-                                                if($ml->ask_hostel==2){
-                                                    $isre_collect = 1;
-                                                    $recollect_against = $ml->id;
-                                                }
-                                                if($ml->ask_hostel==3){
-                                                    $receipt_re = 1;
-                                                    $recollect_against = $ml->id;
-                                                }
-                                            }  
-                                        @endphp
-                                        @if($isre_collect==1)
-                                           <a href="{{ route('student.online-admission.hostel-fee-re', Crypt::encrypt($recollect_against)) }}" class="btn btn-primary">Proceed For Balance Hostel Payment</a>
-                                        @endif
-                                        @if($receipt_re==1)
-                                           <a href="{{ route('student.hostel-receipt-re', Crypt::encrypt($recollect_against)) }}" class="btn btn-primary">Balance Hostel Payment Receipt</a>
-                                        @endif
                                     @endif
 
-                                    
+                                    @php
+                                            $isre_collect = 0;
+                                            $receipt_re = 0;
+                                            if($application->re_payment_flag==1){
+                                                $isre_collect = 1;
+                                            }elseif($application->re_payment_flag==2){
+                                                $receipt_re = 1;
+                                            }
+                                    @endphp
+                                    @if($isre_collect==1)
+                                        <a href="{{ route('student.online-admission.hostel-fee-re', Crypt::encrypt($application->id)) }}" class="btn btn-primary btn-xs">Proceed For Balance Amount</a>
+                                    @endif
+                                    @if($receipt_re==1)
+                                        <a href="{{ route(get_route_guard() . '.hostel-receipt-re', Crypt::encrypt($application->id)) }}" class="btn btn-primary btn-xs">Balance Amount Receipt</a>
+                                    @endif
                                 @endif
                             </td>
                         @endif
