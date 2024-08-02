cedd4852011b67b861cd79bc37837817e3476982
Biswajit Laskar <biswajit2013laskar@gmail.com>
2024-08-02 14:39:16 +0530

    fghgfh

--- app/Http/Controllers/DepartmentUser/HostelAllotmentController.php
+++ app/Http/Controllers/DepartmentUser/HostelAllotmentController.php
@@ -106,7 +106,7 @@ class HostelAllotmentController extends Controller
 
 
     public function changeHostel(Request $request){
-        // dd("ok");
+        dd($request->all());
         $rules = [
             "hos_name"    => "required",
             "hos_room_no" => "required",
